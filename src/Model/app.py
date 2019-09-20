b2eb55d92286a103c0015750bd9ddb492f1304f8
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-09-20 15:52:38 -0700

    To check if flask is running (Basic Skeleton)

--- src/Model/app.py
+++ src/Model/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Api
+from flask import Flask, jsonify, request, render_template
 import flask_mongoalchemy
 # from flask_restful import Api
 # from flask_jwt import JWT
@@ -8,12 +8,40 @@ import flask_mongoalchemy
 app = Flask(__name__)
 
 
-# app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
-app.secret_key = 'vaibhav'  # app.secret_key = app.config['JWT_SECRET_KEY']
-
-
 # app.config['MONGOALCHEMY_DATABASE_URI'] = None
 # app.config['MONGOALCHEMY_TRACK_MODIFICATIONS'] = None
 # app.config['PROPAGATE_EXCEPTIONS'] = None
 # app.config['JWT_BLACKLIST_ENABLED'] = None
 # app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+
+# app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
+# app.secret_key = KEY_STRING  # app.secret_key = app.config['JWT_SECRET_KEY']
+
+userIDs = \
+    [
+        {
+            "username": "FirstUser",
+            "password": "password"
+        }
+    ]
+
+
+@app.route("/")  # Home
+def home():
+    return render_template('html')
+
+
+@app.route('/loginReg')
+def auth():
+    # return render_template('html')
+    return jsonify({'user': userIDs})
+
+
+# To create table
+# @app.before_first_request
+# def create_table():
+#     db.create_all()
+
+
+app.run(port=5000, debug=True)
