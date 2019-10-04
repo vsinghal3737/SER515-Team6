3ef0403b156881e8831a4844f89e80bd324be23e
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-04 12:59:37 -0700

    Update app.py

--- src/Model/app.py
+++ src/Model/app.py
@@ -1,47 +1,25 @@
 from flask import Flask, jsonify, request, render_template
-import flask_mongoalchemy
 # from flask_restful import Api
 # from flask_jwt import JWT
 # from flask_jwt_extended import JWTManager
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../View')
+app.config['PROPAGATE_EXCEPTIONS'] = None
 
-
-# app.config['MONGOALCHEMY_DATABASE_URI'] = None
-# app.config['MONGOALCHEMY_TRACK_MODIFICATIONS'] = None
-# app.config['PROPAGATE_EXCEPTIONS'] = None
+# USER AUTH and User Session [for next sprint]
 # app.config['JWT_BLACKLIST_ENABLED'] = None
 # app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
-
-
 # app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=7)
+
 # app.secret_key = KEY_STRING  # app.secret_key = app.config['JWT_SECRET_KEY']
 
-userIDs = \
-    [
-        {
-            "username": "FirstUser",
-            "password": "password"
-        }
-    ]
 
-
-@app.route("/")  # Home
+# studentDashboard will be a placeholder for home (login/register) [for next sprint]
+@app.route("/")
+@app.route("/StudentView")
 def home():
-    return render_template('html')
-
-
-@app.route('/loginReg')
-def auth():
-    # return render_template('html')
-    return jsonify({'user': userIDs})
-
-
-# To create table
-# @app.before_first_request
-# def create_table():
-#     db.create_all()
+    return render_template('StudentView.html')
 
 
 app.run(port=5000, debug=True)
