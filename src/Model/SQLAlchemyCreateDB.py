afc1325ca179600da570d127568f738b107fa0ad
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-24 21:47:40 -0700

    Changes in DB

--- src/Model/SQLAlchemyCreateDB.py
+++ src/Model/SQLAlchemyCreateDB.py
@@ -1,38 +1,33 @@
-'''
-
-DO NOT RUN THIS FILE
-
-'''
 from app import db, generate_password_hash
 import uuid
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(100), nullable=False)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    UserID = db.Column(db.Integer, primary_key=True)
+    PublicID = db.Column(db.String(100), nullable=False)
+    Username = db.Column(db.String(50), unique=True)
+    Password = db.Column(db.String(80), nullable=False)
     FName = db.Column(db.String(50), nullable=False)
     LName = db.Column(db.String(50))
-    Phone = db.Column(db.Integer)
-    Grade = db.Column(db.Integer)
-    Role = db.Column(db.String(20))
+    Grade = db.Column(db.Integer)  # 1, 2, 3
+    Role = db.Column(db.String(20))  # 'Admin' (only 1), 'Prof', 'Stud'
 
 
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    QuestionID = db.Column(db.Integer, primary_key=True)
     Question = db.Column(db.String(500), nullable=False)
     Answer = db.Column(db.String(100))
-    Grade = db.Column(db.String(50))
-    ProfID = db.Column(db.Integer)
+    Grade = db.Column(db.Integer)
+    ProfPublicID = db.Column(db.Integer)
     SubmittedOn = db.Column(db.Date)
 
 
-class History(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    StudentID = db.Column(db.Integer, nullable=False)
-    HISID_QuestionID = db.Column(db.Integer, nullable=False)
+class HistoryQuestion(db.Model):
+    HisID = db.Column(db.Integer, primary_key=True)
+    HisQuestionID = db.Column(db.Integer, nullable=False)
+    StudPublicID = db.Column(db.Integer, nullable=False)
     AttemptedAns = db.Column(db.String(50))
+    Result = db.Column(db.Boolean)
     SubmittedOn = db.Column(db.Date)
 
 
@@ -45,7 +40,6 @@ def firstRun():
             password=generate_password_hash('password', method='sha256'),
             FName='Vaibhav',
             LName='Singhal',
-            Phone=1234567890,
             Grade=1,
             Role='Admin'
         )
