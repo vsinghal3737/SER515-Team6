c5f6bbe394abdd00af2f501aaaf9c81d6c50a7d8
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-24 21:40:49 -0700

    SQLAlchemyDB

---
+++ src/Model/SQLAlchemyCreateDB.py
@@ -0,0 +1,47 @@
+from app import db, generate_password_hash
+import uuid
+
+
+class User(db.Model):
+    UserID = db.Column(db.Integer, primary_key=True)
+    PublicID = db.Column(db.String(100), nullable=False)
+    Username = db.Column(db.String(50), unique=True)
+    Password = db.Column(db.String(80), nullable=False)
+    FName = db.Column(db.String(50), nullable=False)
+    LName = db.Column(db.String(50))
+    Grade = db.Column(db.Integer)  # 1, 2, 3
+    Role = db.Column(db.String(20))  # 'Admin' (only 1), 'Prof', 'Stud'
+
+
+class Question(db.Model):
+    QuestionID = db.Column(db.Integer, primary_key=True)
+    Question = db.Column(db.String(500), nullable=False)
+    Answer = db.Column(db.String(100))
+    Grade = db.Column(db.Integer)
+    ProfPublicID = db.Column(db.Integer)
+    SubmittedOn = db.Column(db.Date)
+
+
+class HistoryQuestion(db.Model):
+    HisID = db.Column(db.Integer, primary_key=True)
+    HisQuestionID = db.Column(db.Integer, nullable=False)
+    StudPublicID = db.Column(db.Integer, nullable=False)
+    AttemptedAns = db.Column(db.String(50))
+    Result = db.Column(db.Boolean)
+    SubmittedOn = db.Column(db.Date)
+
+
+def firstRun():
+    db.create_all()
+    FirstUser = \
+        User(
+            public_id=str(uuid.uuid4()),
+            username='Admin',
+            password=generate_password_hash('password', method='sha256'),
+            FName='Vaibhav',
+            LName='Singhal',
+            Grade=1,
+            Role='Admin'
+        )
+    db.session.add(FirstUser)
+    db.session.commit()
