41deebbf0a1c4bf304b8ca8c346dd0e3e281fc9d
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-24 21:40:30 -0700

    Dummy Questions

---
+++ src/Model/DummyQuestions.py
@@ -0,0 +1,77 @@
+Questions = {
+    'Q1': {
+        'Question': '5+4=_',
+        'QuestionID': 'Q1',
+        'Answer': '',
+        'Grade': 1,
+        'ProfPublicID': '2',
+        'SubmittedOn': ''
+    },
+    'Q2': {
+        'Question': '9-7=_',
+        'QuestionID': 'Q2',
+        'Answer': '',
+        'Grade': 1,
+        'ProfPublicID': '2',
+        'SubmittedOn': ''
+    },
+    'Q3': {
+        'Question': '_+_=6',
+        'QuestionID': 'Q3',
+        'Answer': '',
+        'Grade': 1,
+        'ProfPublicID': '2',
+        'SubmittedOn': ''
+    },
+    'Q4': {
+        'Question': '3+4=_',
+        'QuestionID': 'Q4',
+        'Answer': '',
+        'Grade': 1,
+        'ProfPublicID': '2',
+        'SubmittedOn': ''
+    },
+    'Q5': {
+        'Question': '_-_=2',
+        'QuestionID': 'Q5',
+        'Answer': '',
+        'Grade': 1,
+        'ProfPublicID': '2',
+        'SubmittedOn': ''
+    }
+}
+
+historyQuestions = {
+    'HQ1': {
+        'HisID': 'HQ1',
+        'StudentID': 'S1',
+        'QuestionID': 'Q4',
+        'AttemptedAns': '9-7=2',
+        'Result': 'Pass',
+        'Date': '2019-10-24 17:55:21'
+    },
+    'HQ1': {
+        'HisID': 'HQ1',
+        'StudentID': 'S1',
+        'QuestionID': 'Q4',
+        'AttemptedAns': '9-7=3',
+        'Result': 'Pass',
+        'Date': '2019-10-24 17:55:21'
+    },
+    'HQ1': {
+        'HisID': 'HQ1',
+        'StudentID': 'S1',
+        'QuestionID': 'Q4',
+        'AttemptedAns': '9-7=2',
+        'Result': 'Pass',
+        'Date': '2019-10-24 17:55:21'
+    },
+    'HQ1': {
+        'HisID': 'HQ1',
+        'StudentID': 'S1',
+        'QuestionID': 'Q4',
+        'AttemptedAns': '9-7=2',
+        'Result': 'Pass',
+        'Date': '2019-10-24 17:55:21'
+    }
+}
