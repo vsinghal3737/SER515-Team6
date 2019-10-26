d396d4824ee08f6d7f3262541019eb8e8bb89268
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-26 13:33:05 -0700

    Integration Login BackEnd and FrontEnd Phase 1

--- src/Model/app.py
+++ src/Model/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify, request, render_template, make_response
+from flask import Flask, jsonify, request, render_template, make_response, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.dialects.postgresql import UUID
+from flask_login import LoginManager, UserMixin
 import os
 import jwt
 import datetime
 from functools import wraps
-
 
 # from user import UserDB
 # from security import authenticate, identity
@@ -19,24 +17,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(str(os.path.abspat
 
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route("/")
 def home():
-    return render_template('dashboard.html', retry=False)
+    return render_template('dashboard.html')
 
 
-@app.route("/StudentView")
-def studentView():
-    return render_template('StudentView.html')
+@app.route('/LogReg', methods=['POST', 'GET'])
+def loginOption():
+    return render_template('login.html') if request.form['submit'] == 'login' \
+        else render_template('signup.html') if request.form['submit'] == 'signup' \
+        else render_template('dashboard.html')
 
 
-@app.route("/TeacherView")
-def teacherDashboard():
-    return render_template('TeacherView.html')
+# @app.route("/StudentView")
+# def studentView():
+#     return render_template('StudentView.html')
 
 
-# Login Api
-@app.route("/login", methods=['POST'])
+# @app.route("/TeacherView")
+# def teacherDashboard():
+#     return render_template('TeacherView.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     data = request.get_json()
     username = data['username']
@@ -55,7 +67,7 @@ def login():
         elif userRole == "Admin":
             AllUsers = UserDB.getAllUsers(usr.username)
             return render_template("AdminView", AllUsers=AllUsers)
-    return render_template('dashboard.html', retry=True)
+    return render_template('dashboard.html')
 
 
 @app.route("/register")
@@ -64,4 +76,5 @@ def register():
 
 
 if __name__ == '__main__':
+    from SQLAlchemyCreateDB import User
     app.run(port=5000, debug=True)
