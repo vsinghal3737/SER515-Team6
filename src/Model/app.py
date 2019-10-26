98d687cc0ed711237caabd4e791a7a4c3d10169b
vsinghal3737 <vsinghal3737@users.noreply.github.com>
2019-10-26 16:37:13 -0700

    US#70 Task #84 & #89 | US#71 Task #106
    Completed: US#70 Task #84 & #89
    Ready For Test: US#71 Task #106

--- src/Model/app.py
+++ src/Model/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 import os
 import jwt
 import datetime
 from functools import wraps
-
-from security import authenticate, identity
 
 
 app = Flask(__name__, template_folder='../View', static_folder='../Controller')
@@ -18,12 +16,12 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'dashboard'
+login_manager.login_view = 'home'
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return identity(int(user_id))
+    return Security.identity(int(user_id))
 
 
 @app.route("/")
@@ -38,21 +36,27 @@ def loginOption():
         else render_template('dashboard.html')
 
 
-@app.route("/login", methods=['POST', 'GET'])
+@app.route("/Login")
+@app.route("/login")
+def Login():
+    return render_template('login.html')
+
+
+@app.route("/login", methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
-    user = authenticate(username, password)
-    # if user:
-    #     login_user(user)
-    #     if user.Role == 'stud':
-    #         return render_template('StudentView.html')
-    #     elif user.Role == "Prof":
-    #         return render_template("TeacherView")
-    #     elif user.Role == "Admin":
-    #         return render_template("AdminView")
-    # return render_template('dashboard.html')
-    return ' '.join([user.Username, user.Password])
+    user = Security.authenticate(username, password)
+    if user:
+        login_user(user)
+        if user.Role == 'Stud':
+            return render_template('StudentView.html')
+        elif user.Role == "Prof":
+            return render_template("TeacherView")
+        elif user.Role == "Admin":
+            return render_template("AdminView")
+    return render_template('dashboard.html')
+    # return str(user.Username)
 
 
 @app.route("/signup", methods=['POST', 'GET'])
@@ -61,7 +65,14 @@ def register():
     return ''
 
 
+@app.route('/check')
+@login_required
+def check():
+    return '{} {} {}'.format(current_user.Username, current_user.Grade, current_user.Role)
+
+
 @app.route("/logout")
+@app.route("/Logout")
 @login_required
 def logout():
     logout_user()
@@ -69,4 +80,5 @@ def logout():
 
 
 if __name__ == '__main__':
+    from security import Security
     app.run(port=5000, debug=True)
--- src/Model/security.py
+++ src/Model/security.py
@@ -2,16 +2,18 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from user import User
 
 
-def authenticate(username, password):
-    user = User.find_by_username(username)
-    if user and check_password_hash(user.Password, password):
-        return user
+class Security:
+    @classmethod
+    def authenticate(cls, username, password):
+        user = User.find_by_username(username)
+        if user and check_password_hash(user.Password, password):
+            return user
 
+    @classmethod
+    def identity(cls, user_id):
+        return User.find_by_id(user_id)
 
-def identity(user_id):
-    return User.find_by_id(user_id)
-
-
-def FirstTime(username, password):
-    newPassword = generate_password_hash(password, method='sha256')
-    User.NewPassword(username, newPassword)
+    @classmethod
+    def FirstTime(cls, username, password):
+        newPassword = generate_password_hash(password, method='sha256')
+        User.NewPassword(username, newPassword)
