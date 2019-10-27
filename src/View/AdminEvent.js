72d4766a0842b2b6c3dfe3b0f4191353c684e31f
MayankBatra005 <er.mayank005@gmail.com>
2019-10-27 14:54:29 -0700

    Admin UI and javascript for US 92

---
+++ src/View/AdminEvent.js
@@ -0,0 +1,19 @@
+function saveChanges()
+{
+	
+	var selectOption1=document.getElementById("select_tag1").value;
+	var selectOption2=document.getElementById("select_tag2").value;
+	var selectOption3=document.getElementById("select_tag3").value;
+	
+	
+}
+
+function search()
+{
+	
+	
+	var searchValue=document.getElementById("search_text").value;
+	console.log(searchValue);
+	//document.getElementById("1").innerHTML=searchValue;
+	
+}
