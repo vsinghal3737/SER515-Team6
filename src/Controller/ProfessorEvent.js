82547fc93e56a48e965ca5ed2f834708602fda0c
Suraj Atmakuri <surajatmakuri@hotmail.com>
2019-11-06 20:29:00 -0700

    US#113 T#135 refactored code

--- src/Controller/ProfessorEvent.js
+++ src/Controller/ProfessorEvent.js
@@ -12,10 +12,11 @@ function drag(ev) {
 function drop(ev, el) {
         ev.preventDefault();
         var id = ev.dataTransfer.getData("text");
-
+        //var isLeft = "example1" == id || "example2" == id;
         var childNode = document.getElementById(id).childNodes[1];
         var nodeCopy = childNode.cloneNode(true);
-
+        //nodeCopy.id = id + ev.target.id;
+        // clean target space if needed 
         nodeCopy.style.fontSize = "45px";
         el.appendChild(nodeCopy);
         
