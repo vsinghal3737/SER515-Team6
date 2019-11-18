6fff0f4c12748b83d699a15597689b63ddac63dd
Suraj Atmakuri <surajatmakuri@hotmail.com>
2019-11-17 17:24:54 -0700

    added drop function US #113 T#135

--- src/Controller/ProfessorEventGrade4.js
+++ src/Controller/ProfessorEventGrade4.js
@@ -24,3 +24,13 @@ function drag(ev) {
    
 }
 
+function drop(ev, el) {
+        ev.preventDefault();
+        var id = ev.dataTransfer.getData("text");
+        var childNode = document.getElementById(id).childNodes[1];
+        var nodeCopy = childNode.cloneNode(true);
+        nodeCopy.style.fontSize = "45px";
+        el.appendChild(nodeCopy);
+        ev.stopPropagation();
+        return false;
+}
